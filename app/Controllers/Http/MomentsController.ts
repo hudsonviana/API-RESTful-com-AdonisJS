87046229bde8c52348ec140964092f56ed0d9c9c
hudsonviana <hudson1206@gmail.com>
2023-03-05 16:58:52 -0300

    inserindo dados no BD

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,9 +1,26 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+
+import Moment from 'App/Models/Moment';
 
 export default class MomentsController {
-  public async store() {
+  public async store({ request, response }: HttpContextContract) {
+    const body = request.body();
+    const moment = await Moment.create(body);
+    response.status(201);
     return {
-      msg: 'Deu certo, valeu demais!',
+      message: 'ok',
+      data: moment,
     };
   }
 }
+
+// https://www.youtube.com/watch?v=y8XfJJYhXPE
+// PAREI NO TEMPO: 38:39
+
+/**
+ * GET|HEAD   /api/moments ────────────── moments.index ›   MomentsController.index
+ * POST       /api/moments ────────────── moments.store ›   MomentsController.store
+ * GET|HEAD   /api/moments/:id ────────── moments.show ›    MomentsController.show
+ * PUT|PATCH  /api/moments/:id ────────── moments.update ›  MomentsController.update
+ * DELETE     /api/moments/:id ────────── moments.destroy › MomentsController.destroy
+ */
--- start/routes.ts
+++ start/routes.ts
@@ -7,10 +7,10 @@ Route.group(() => {
 
   Route.resource('/moments', 'MomentsController').apiOnly();
   /**
-   * GET|HEAD   /api/moments ────────────── moments.index › MomentsController.index
-   * POST       /api/moments ────────────── moments.store › MomentsController.store
-   * GET|HEAD   /api/moments/:id ────────── moments.show › MomentsController.show
-   * PUT|PATCH  /api/moments/:id ────────── moments.update › MomentsController.update
+   * GET|HEAD   /api/moments ────────────── moments.index ›   MomentsController.index
+   * POST       /api/moments ────────────── moments.store ›   MomentsController.store
+   * GET|HEAD   /api/moments/:id ────────── moments.show ›    MomentsController.show
+   * PUT|PATCH  /api/moments/:id ────────── moments.update ›  MomentsController.update
    * DELETE     /api/moments/:id ────────── moments.destroy › MomentsController.destroy
    */
 }).prefix('api');
