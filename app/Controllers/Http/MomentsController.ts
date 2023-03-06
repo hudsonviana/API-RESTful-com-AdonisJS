ceec8fe405e92416c38c77f8ca044e948cafd98a
hudsonviana <hudson1206@gmail.com>
2023-03-05 23:13:21 -0300

    Atualizando um registro

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -44,6 +44,33 @@ export default class MomentsController {
     };
   }
 
+  public async update({ params, request }: HttpContextContract) {
+    const body = request.body();
+    const moment = await Moment.findOrFail(params.id);
+
+    moment.title = body.title;
+    moment.description = body.description;
+
+    if (!moment.image || moment.image !== body.image) {
+      const image = request.file('image', this.validationOptions);
+
+      if (image) {
+        const imageName = `${uuidv4()}.${image.extname}`;
+        await image.move(Application.tmpPath('uploads'), {
+          name: imageName,
+        });
+        moment.image = imageName;
+      }
+    }
+
+    await moment.save();
+
+    return {
+      message: 'Dados atualizados com sucesso',
+      data: moment,
+    };
+  }
+
   public async destroy({ params }: HttpContextContract) {
     const moment = await Moment.findOrFail(params.id);
 
